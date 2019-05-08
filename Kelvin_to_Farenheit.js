ba2e4462c642a3fdeef1e6ffb56b09e0114f596c
Andrew Blair <blair.d.andrew@gmail.com>
2019-05-08 23:28:16 +0100

    udemy practice

---
+++ Kelvin_to_Farenheit.js
@@ -0,0 +1,11 @@
+var kelvinTemp = 301;
+
+// celsius is 273.15 degrees lower than kelvin so taking it away from the kelvinTemp variable
+var celsius = kelvinTemp - 273.15;
+
+var farenheitTemp = celsius *(9/5) + 32
+var farenheit = Math.floor(farenheitTemp);
+
+
+// console.log(Math.floor(farenheitTemp));
+console.log(`The temperature is ${farenheit} degrees F`);

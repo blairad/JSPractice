aceb603899b1171a664bb4e93f56e81808261c7d
Andrew Blair <blair.d.andrew@gmail.com>
2019-08-23 16:47:16 +0100

    8ball works in browser but not in terminal :/

--- sillyFunction.js
+++ sillyFunction.js
@@ -12,18 +12,51 @@
 
 // console.log(cycleWeather('sunny'))
 
-var noOfBikes = [ 0, 1, 5, 6, 3, 8, 2, 7, 4,0]
+// var noOfBikes = [ 0, 1, 5, 6, 3, 8, 2, 7, 4,0]
 
-for(i =0; i < noOfBikes.length; i++ ) {
-	if (noOfBikes[i] < 1) {
-	console.log("For the love of god please buy a bike")
-} else if (noOfBikes[i] < 4) {
-	console.log("maybe buy another one")	
-} else if (noOfBikes[i] <= 6) {
-	console.log("go on, just one more")
+// for(i =0; i < noOfBikes.length; i++ ) {
+// 	if (noOfBikes[i] < 1) {
+// 	console.log("For the love of god please buy a bike")
+// } else if (noOfBikes[i] < 4) {
+// 	console.log("maybe buy another one")	
+// } else if (noOfBikes[i] <= 6) {
+// 	console.log("go on, just one more")
+// } else  {
+//         console.log("good work")
+// }
+// }
+const userName = prompt('What is your name?');
+
+if (userName){
+        console.log("hello, " + userName + "!");
+} else {
+        console.log("hi")
+}
+const userQuestion = prompt("Please ask a question")
+
+console.log("hello, " + userName + "you asked" + userQuestion)
+
+const randomNumber = Math.floor(Math.random() * 8);
+const eightBall = "";
+
+if (randomNumber === 1)  {
+	console.log ('Better not tell you now.') 
+} else if (randomNumber === 2) {
+	console.log('Concentrate and ask again.')
+} else if (randomNumber === 3) {
+	console.log('Reply hazy try again')
+} else if (randomNumber === 4) {
+	console.log('Cannot predict now') 
+} else if (randomNumber === 5) {
+	console.log('My reply is no.')
+} else if (randomNumber === 6) {
+	console.log('My sources say no')
+} else if (randomNumber === 7) {
+	console.log('Outlook not so good')
+} else if (randomNumber === 8) {
+	console.log('Signs point to yes')
 } else  {
-        console.log("good work")
-}
+	console.log('could you repeat the question?')
 }
 
-// console.log(noOfBikes(3))
+console.log('The eight ball answer: ' + eightBall);

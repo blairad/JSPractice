06adb43c4da32fb8415fa5ce726ec8d6f1d8743e
Andrew Blair <blair.d.andrew@gmail.com>
2019-10-29 12:26:40 +0000

    think i got time working. will css in a bit

--- eloquentJS.js
+++ eloquentJS.js
@@ -1,5 +1,5 @@
-for ( var x = "#"; x.length < 7; x += "#")
-console.log(x);
-// above is getting to form the pyramid. below is what i did thinking i'd get the same result
-for(var x = 0; x < 8; x++)
-console.log(x);
+// for ( var x = "#"; x.length < 7; x += "#")
+// console.log(x);
+// // above is getting to form the pyramid. below is what i did thinking i'd get the same result
+// for(var x = 0; x < 8; x++)
+// console.log(x);

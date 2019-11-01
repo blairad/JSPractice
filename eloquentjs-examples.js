dd10faf7bde8f8294eb0f6017213a3fa1e5e9e51
Andrew Blair <blair.d.andrew@gmail.com>
2019-11-01 15:38:27 +0000

    the landscape function

--- eloquentjs-examples.js
+++ eloquentjs-examples.js
@@ -7,3 +7,27 @@ var power = function(base, exponent) {
   console.log(power(2, 10))
 
   // returns 2 * 2 * 2 * 2 ect until it will reach 1024
+
+  var landscape = function() {
+    var result = "";
+    var flat = function(size){
+      for(var count = 0; count < size; count++)
+     result += "_";
+    };
+    var mountain = function(size){
+      result += "/";
+      for (var count = 0; count < size; count++)
+        result += "'";
+      result += "\\";
+    }
+    
+    flat(3);
+    mountain(4);
+    flat(3);
+    mountain(1);
+    flat(1);
+    mountain(2)
+    return result;
+  };
+  console.log(landscape())
+//"___/''''\___/'\_/''\"  
